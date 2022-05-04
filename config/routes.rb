e905ba1b89a1b3ec14a5018798ade862954f7414
Syre <chestersyron.damian14@outlook.com>
2022-05-04 19:33:44 +0800

    added bootstrap and simple styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
+  get 'project/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root "hello#index"
-  
-  get "/hello", to: "hello#index"
+  root "project#index"
 
   # Defines the root path route ("/")
   # root "articles#index" 
